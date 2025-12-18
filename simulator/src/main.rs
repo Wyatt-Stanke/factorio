1edd395d059e2b4fa99c5805ca942a7de4146bdd
Wyatt Stanke <47758296+Wyatt-Stanke@users.noreply.github.com>
2025-12-18 16:51:25 +0000

    BeltType methods for throughput calculations

--- simulator/src/main.rs
+++ simulator/src/main.rs
@@ -15,12 +15,21 @@ enum BeltType {
 }
 
 impl BeltType {
-    fn items_per_second(&self) -> f32 {
+    pub fn tiles_traveled_per_second(&self) -> f32 {
         match self {
-            BeltType::Regular => 1.0,
-            BeltType::Fast => 1.5,
-            BeltType::Express => 2.0,
-            BeltType::Turbo => 3.0,
+            BeltType::Regular => 1.875,
+            BeltType::Fast => 3.75,
+            BeltType::Express => 5.625,
+            BeltType::Turbo => 7.5,
+        }
+    }
+
+    pub fn item_throughput_per_second_one_lane(&self) -> f32 {
+        match self {
+            BeltType::Regular => 7.5,
+            BeltType::Fast => 15.0,
+            BeltType::Express => 22.5,
+            BeltType::Turbo => 30.0,
         }
     }
 }
@@ -30,9 +39,14 @@ struct BeltLane {
     // The tuple stores the item and its relative position on the belt (0.0 to 1.0).
     // TODO: Might be more efficent to use fixed point instead of float since it's ranged. This would
     // also allow for us to make the whole tuple non-zero which would allow for the option to be zero.
+    // The way belts are simulated in the game is that items can be on one of 256 discrete positions on the belt.
+    // To see more, check
+    // - Factorio wiki/Belt transport system
+    // - Factorio wiki/Transport Belts/Physics
     // This uses a fixed-size array for performance reasons.
     items: [Option<(Item, f32)>; 5],
     belt_type: BeltType,
+    next_belt: Option<Box<BeltLane>>,
 }
 
 struct Belt {

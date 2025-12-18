dc2e1065f330f31e2a0349b384d70c6a3d615e8b
Wyatt Stanke <47758296+Wyatt-Stanke@users.noreply.github.com>
2025-12-18 16:17:47 +0000

    Start

--- simulator/src/main.rs
+++ simulator/src/main.rs
@@ -1,3 +1,22 @@
+use std::num::NonZeroUsize;
+
+// Temp
+type Item = NonZeroUsize;
+
+struct BeltLane {
+    // A belt lane can have a maximum of 5 items on it at any time.
+    // The tuple stores the item and its relative position on the belt (0.0 to 1.0).
+    // TODO: Might be more efficent to use fixed point instead of float since it's ranged. This would
+    // also allow for us to make the whole tuple non-zero which would allow for the option to be zero.
+    // This uses a fixed-size array for performance reasons.
+    items: [Option<(Item, f32)>; 5],
+}
+
+struct Belt {
+    left_lane: BeltLane,
+    right_lane: BeltLane,
+}
+
 fn main() {
     println!("Hello, world!");
 }
